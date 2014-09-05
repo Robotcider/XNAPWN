8cdb4a81690bddd4bf5fbd6d4a79656552d16598
Robotcider <tonyortiz-18@hotmail.com>
2014-09-05 00:03:41 -0700

    New Background and new HUD class

--- WeakSven/WeakSven/HUD.cs
+++ WeakSven/WeakSven/HUD.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,5 +10,25 @@ namespace WeakSven
 {
 	class HUD
 	{
+
+		public HUD()
+		{
+
+		}
+
+		public void Update(GameTime gameTime)
+		{
+
+		}
+
+		public void Load(ContentManager Content)
+		{
+
+		}
+
+		public void Draw(SpriteBatch spriteBatch)
+		{
+
+		}
 	}
 }
